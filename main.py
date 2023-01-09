8312a9c59b96bc964f893f2eece36016ae42cea7
Juan Sidberry <juan.sidberry@insightglobal.com>
2023-01-09 12:36:24 -0500

    tested basic functionality on yahoo

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+browser = webdriver.Firefox()
+
+browser.get('http://www.yahoo.com')
+assert 'Yahoo' in browser.title
+
+elem = browser.find_element(By.NAME, 'p')  # Find the search box
+elem.send_keys('seleniumhq' + Keys.RETURN)
+
+browser.quit()
 
 def main():
     pass
